fc6b3f345a94534b38e40550a9a384c6666387a0
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2018-11-06 09:43:27 -0700

    Update chitter.php

--- chitter.php
+++ chitter.php
@@ -12,7 +12,7 @@ $language = new LanguageClient([
 
 $connection = new TwitterOAuth('NCRsbohuk10DznHImq8UY9OOK', '6lDZEYjQtntV3XX65iqfk5WUk6HLOurPi88Ebt6lFwPDyjKWwk', '1059833903286910977-O0YedZohxiLVH2RpfP1qH6cGoFxKYm', 'LbDDM3WkXtD7buL2BFPGDW8f20zWAvBQhqzi7NAkv4XZb');
 $content = $connection -> get("account/verify_credentials");
-$tweets = $connection -> get("search/tweets", ["q" => "from:spidamitchell"]);
+$tweets = $connection -> get("search/tweets", ["q" => $_POST['query']]);
 
 $totalSentimentScore = 0;
 $totalMagnitudeScore = 0;
