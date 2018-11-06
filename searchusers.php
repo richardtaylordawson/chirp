ee2f5e2ba3ff077986ad797e8f8b7a70a538a848
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2018-11-06 11:10:50 -0700

    Update searchusers.php

--- searchusers.php
+++ searchusers.php
@@ -6,6 +6,6 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 $connection = new TwitterOAuth('NCRsbohuk10DznHImq8UY9OOK', '6lDZEYjQtntV3XX65iqfk5WUk6HLOurPi88Ebt6lFwPDyjKWwk', '1059833903286910977-O0YedZohxiLVH2RpfP1qH6cGoFxKYm', 'LbDDM3WkXtD7buL2BFPGDW8f20zWAvBQhqzi7NAkv4XZb');
 $content = $connection -> get("account/verify_credentials");
-$users = $connection -> get("search/users", ["q" => $_POST['query'], "count" => 20]);
+$users = $connection -> get("users/search", ["q" => $_POST['query'], "count" => 20]);
 
 echo json_encode($users);
