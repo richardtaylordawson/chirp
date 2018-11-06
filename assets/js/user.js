53e1bf3e99a42e86225629930852ce41807d4746
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2018-11-06 12:56:20 -0700

    asdf

---
+++ assets/js/user.js
@@ -0,0 +1,25 @@
+document.getElementById('analyze-button').addEventListener('click', () => {
+    const button = document.getElementById('analyze-button');
+
+    button.disabled = true;
+    button.textContent = 'Analyzing...';
+
+    $.ajax({
+        url: '../../chirp.php',
+        type: 'post',
+        data: {
+            query: document.getElementById('searchUserOneInput').value
+        },
+        dataType: 'json',
+        success: (response) => {
+            console.log(response);
+        },
+        error: () => {
+            console.log('error!');
+        },
+        complete: () => {
+            button.textContent = 'Analyze';
+            button.disabled = false;
+        }
+    });
+});
